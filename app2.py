9c4f7feaddec282deff2f686188f01e99f500dbe
Akash Alaria <79797975+AkashAlaria@users.noreply.github.com>
2024-05-07 19:30:07 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,9 +2,6 @@ import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-client_id = 'c69900443e754f59a83f7d7a1baa8683'
-client_secret = 'd256cf44440b4d49af6de1a14d1f71ea'
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri='http://localhost:8080',
